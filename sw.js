66bc9e2e0dc508065cdf742949b152efe0c3ba06
Sagar712 <74832706+Sagar712@users.noreply.github.com>
2021-04-23 20:27:56 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ self.addEventListener("install", e => {
             return cache.addAll(["./", "./app.js", "./style.css", "./mask294.png",
              "./contact.html", "./howto.html", "./otherstyle.css",
               "./output.html", "./output.js", "./backup.html", "./backup.js", "./backstyle.css",
-                "./import-export/import-export.css", "./import-export/import-export.html", "/import-export/import-export.js"]);
+                "./import-export/import-export.css", "./import-export/import-export.html", "./import-export/import-export.js"]);
         })
     );
 });
