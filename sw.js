b842806c40dafacc00aead8d5c6c260ef9d75f63
Sagar712 <74832706+Sagar712@users.noreply.github.com>
2021-07-24 09:47:29 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener("install", e => {
     e.waitUntil(
-        caches.open("secretscript_v6").then(cache => {
+        caches.open("script_files").then(cache => {
             return cache.addAll(["./", "./app.js", "./style.css", "./mask294.png",
              "./contact.html", "./howto.html", "./otherstyle.css",
               "./output.html", "./output.js", "./backup.html", "./backup.js", "./backstyle.css",
